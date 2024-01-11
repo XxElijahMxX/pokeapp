5959a150b3cd8ec35a459de4f5804c925fb85857
Sheridan Melton <99241310+XxElijahMxX@users.noreply.github.com>
2024-01-11 13:06:03 -0500

    added base url pokemon api to log pokemon in the console

--- src/context/global.js
+++ src/context/global.js
@@ -1,21 +1,30 @@
-import React, { createContext, useContext, useReducer } from "react"
+import React, { createContext, useContext, useEffect, useReducer } from "react"
 
 const GlobalContext = createContext();
 
+//actions
+const LOADING = 'LOADING';
+
 //reducers
 const reducer = (state, action) => {
+
+    switch (action.type) {
+        case LOADING:
+            return {...state, loading: true}
+    }
    return state; 
 }
 
 export const GlobalProvider = ({ children }) => {
 
     // This is where the base URL will go
+    const baseUrl = 'https://pokeapi.co/api/v2/'
 
 
     // This is the initial state of the data being passed through
     const initialState = {
-        allPKMS: [],
-        PKMS: {},
+        allPKM: [],
+        PKM: {},
         PKMdata: [],
         search: [],
         next: "",
@@ -23,6 +32,18 @@ export const GlobalProvider = ({ children }) => {
     };
 
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    const allPKM = async () => {
+        dispatch({ type: 'LOADING'})
+        const res = await fetch (`${baseUrl}pokemon?limit=151`);
+        const data = await res.json();
+        console.log(data);
+    };
+
+    useEffect(() => {
+        allPKM();
+    }, []);
+
     return (
         <GlobalContext.Provider value={{
             ...state,
